04d7121f00b53717bd137c26a51a3c045c109bf8
Kevin Ghio <kevin@MacBook-Pro-de-Kevin.local>
2018-07-30 20:30:18 -0300

    clase 1 y 2

---
+++ clase01.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="/css/master.css">
+    </head>
+    <body>
+        <pre>
+        <?php
+            $usuario = [
+                'name' => 'Kevin',
+                'lastname' => 'Ghio',
+                'phone' => 'Mi telefono',
+                'pass' => 'Pass'
+            ];
+            $usuario[] = '788';
+
+
+            if ($usuario) {
+                var_dump($usuario);
+            }
+
+        ?>
+    </body>
+
+
+    <?php
+echo "asdasadasdsaddsdssadads";
+    ?>
+
+
+
+
+
+
+</html>
---
+++ clase02.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<?php
+$color = 'blue';
+
+ ?>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body style="background-color:<?=isset($color)?$color:'white' ?>">
+        <li></li>
+    </body>
+</html>
+
+
+<?php
+$color = 'FSWD';
+switch ($color) {
+    case 'FSWD':
+        echo "El color es azul";
+        break;
+    default:
+        echo "Es otro color ! ";
+        break;
+}
+
+
+
+
+
+ ?>
